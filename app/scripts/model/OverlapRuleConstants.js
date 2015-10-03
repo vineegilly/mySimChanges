8776f8170152e573d938754ce35782dbd8b4aa3b
faiyazuddeen <m.faiyaz123@gmail.com>
2015-10-03 19:30:55 +0530

    remaining table content added accordingly

--- app/scripts/model/OverlapRuleConstants.js
+++ app/scripts/model/OverlapRuleConstants.js
@@ -2,14 +2,43 @@
 /*
  beetle,grass,earthworm,flower,hawk,
  mushroom,snake,tree, mouse,butterfly,raccoon,
- deer,rabbit,cayote,frog,songbird
+ deer,rabbit,coyote,frog,songbird
  */
 
 var OverlapRuleConstants = {
   beetle: {
-    prey: [ "songbird", "mouse" ], predator: [ "grass", "flower", "tree" ]
+    prey: [ "songbird", "mouse","frog" ], predator: [ "grass", "flower", "tree" ]
   },
-  grass: {}
+  grass: {
+      prey: ["beetle","mouse","deer","rabbit","songbird"], predator:[]
+  },
+    flower: {
+        prey:["beetle","mouse","deer","rabbit","butterfly"],predator:[]
+    },
+
+    tree: {
+        prey :["beetle","mouse","deer","butterfly","songbird","raccoon"],predator: []
+    },
+
+    butterfly:{
+        prey:["songbird","frog"],predator:["flower","tree"]
+    },
+
+    rabbit:  {
+        prey: ["coyote","hawk"],predator:["mushroom","grass","flower"]
+    },
+
+    deer:{
+        prey:["coyote"],predator:["mushroom","grass","flower","tree"]
+    },
+
+    songbird:{
+        prey:["snake","hawk"],predator:["beetle","grass","earthworm","tree","butterfly"]
+    },
+
+    mouse:{
+        prey:["raccoon","snake","hawk","coyote"],predator:["mushroom","grass","tree","beetle"]
+    }
 
 };
 
